6b33dc22702b7c5f9f5a1d5fcca1015ddeabd9f8
Sam <samratbiswas986@gmail.com>
2023-07-25 02:52:56 +0530

    Added Camera Controller

---
+++ Assets/Scripts/BasicFollowCamera.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class BasicFollowCamera : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject T;
+    public float speed = 1.5f;
+    public Vector3 offset;
+
+   
+    private void Update() {
+        offset = target.transform.position + new Vector3(0, 3, -6);
+        
+        if (InputManager.Instance.GetBreakInput()) {
+            offset = Vector3.Lerp(offset, target.transform.position + new Vector3(0, 3, 12), 0.25f);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        transform.LookAt(target.transform);
+        float _carMove = Mathf.Abs(Vector3.Distance(transform.position, offset) * speed);
+        transform.position = Vector3.MoveTowards(transform.position, offset, _carMove * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [Header("Follow Target")] 
+    [SerializeField] private Transform playerCar;
+    [SerializeField] private Transform lookAtTarget;
+    [SerializeField] private float cameraSpeed;
+    [SerializeField] private Vector3 offset;
+
+
+    /// <summary>
+    /// Private Variables.
+    /// </summary>
+    private Rigidbody playerRb;
+    
+    void Start() {
+        playerRb = playerCar.GetComponent<Rigidbody>();
+    }
+
+    
+    void FixedUpdate() {
+        Vector3 _playerForward = (playerRb.velocity + playerCar.transform.forward).normalized;
+
+        transform.position = Vector3.Lerp(
+            transform.position,
+            playerCar.position + playerCar.transform.TransformVector(offset) + _playerForward * (-5.5f),
+            cameraSpeed * Time.deltaTime
+        );
+        
+        transform.LookAt(lookAtTarget);
+
+    }
+}
